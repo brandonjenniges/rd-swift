d70310e90be471b8bc24c110395357beb034a46b
Brandon Jenniges <steveholt55@gmail.com>
2016-01-14 14:16:44 -0600

    Makes control pad a little more dynamic sized

--- GameElements/Control/ControlPadPaintCodeImage.swift
+++ GameElements/Control/ControlPadPaintCodeImage.swift
@@ -14,20 +14,43 @@ class ControlPadPaintCodeImage: UIImageView {
         return image
     }
     
-    internal class func draw(canvas: CGRect) {
+    internal class func draw(frame: CGRect) {
+        
+        let context = UIGraphicsGetCurrentContext()
+        
         //// Color Declarations
         let color = UIColor(red: 0.000, green: 0.477, blue: 0.969, alpha: 1.000)
         
         //// Rectangle Drawing
-        let rectanglePath = UIBezierPath(rect: CGRectMake(0, 0, canvas.width, canvas.height))
+        let rectanglePath = UIBezierPath(rect: CGRectMake(frame.minX, frame.minY, frame.width, frame.height))
         color.setFill()
         rectanglePath.fill()
         
         
         //// Rectangle 2 Drawing
-        let rectangle2Path = UIBezierPath(rect: CGRectMake(0, 0, canvas.width, canvas.height))
+        let rectangle2Path = UIBezierPath(rect: CGRectMake(frame.minX, frame.minY, frame.width, frame.height))
         UIColor.whiteColor().setStroke()
-        rectangle2Path.lineWidth = 2
+        rectangle2Path.lineWidth = 4
         rectangle2Path.stroke()
+        
+        
+        //// Polygon Drawing
+        CGContextSaveGState(context)
+        CGContextTranslateCTM(context, frame.minX + 0.37625 * frame.width, frame.minY + 0.83929 * frame.height)
+        CGContextRotateCTM(context, -90 * CGFloat(M_PI) / 180)
+        
+        let polygonPath = UIBezierPath()
+        polygonPath.moveToPoint(CGPointMake(12.38, 0))
+        polygonPath.addLineToPoint(CGPointMake(23.09, 18.56))
+        polygonPath.addLineToPoint(CGPointMake(1.66, 18.56))
+        polygonPath.closePath()
+        UIColor.whiteColor().setFill()
+        polygonPath.fill()
+        UIColor.whiteColor().setStroke()
+        polygonPath.lineWidth = 2
+        polygonPath.stroke()
+        
+        CGContextRestoreGState(context)
+
     }
 }
