432b97ba20365dc5c3460d22c43616b2ec646072
Brandon Jenniges <steveholt55@gmail.com>
2016-01-20 20:37:52 -0600

    Adds crashlytics to project

--- iOS/AppDelegate.swift
+++ iOS/AppDelegate.swift
@@ -3,6 +3,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -12,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         print("iOS")
+        Fabric.with([Crashlytics.self])
         return true
     }
 }
